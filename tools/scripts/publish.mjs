fec1dfcd2a39a8e90bad0588dfa890076a667df7
Jesper Orb <github@jesperorb.com>
2023-10-08 11:04:57 +0200

    chore: fix publish command

--- tools/scripts/publish.mjs
+++ tools/scripts/publish.mjs
@@ -7,7 +7,7 @@
  * You might need to authenticate with NPM before running this script.
  */
 
-import { readCachedProjectGraph } from '@nx/devkit';
+import devKit from '@nx/devkit';
 import { execSync } from 'child_process';
 import { readFileSync, writeFileSync } from 'fs';
 import chalk from 'chalk';
@@ -30,7 +30,7 @@ invariant(
 	`No version provided or version did not match Semantic Versioning, expected: #.#.#-tag.# or #.#.#, got ${version}.`,
 );
 
-const graph = readCachedProjectGraph();
+const graph = devKit.readCachedProjectGraph();
 const project = graph.nodes[name];
 
 invariant(
